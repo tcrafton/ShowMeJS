7250c1473efcea17cdb099b79f1e37f2078e2512
Tommy Crafton <twcrafton@gmail.com>
2016-02-15 14:31:03 -0600

    Added initial layout and recursion example

--- ShowMeJS/js/main.js
+++ ShowMeJS/js/main.js
@@ -1,1 +1,34 @@
-﻿
+﻿var ShowMeJS = (function () {
+    "use strict";
+    
+    var publicAPI;
+
+    function displayCode(codeToShow) {
+        $("#codeDisplay").html(Util.showCode(codeToShow));
+        var precode = document.getElementById('codeDisplay');
+        Prism.highlightElement(precode);
+    }
+
+    function init() {
+        // add event handlers
+        $("#no_recursion").on("click", (function () {
+            return displayCode(RecursionExample.getTasks);
+        }));
+
+        $("#with_recursion").on("click", (function () {
+            return displayCode(RecursionExample.getTasksWithRecursion);
+        }));
+    }
+
+    publicAPI = {
+        init: init
+    };
+
+    return publicAPI;
+
+})();
+
+
+$(document).ready(function () {
+    ShowMeJS.init();   
+});
---
+++ ShowMeJS/js/recursion.js
@@ -0,0 +1,87 @@
+﻿// For each item in the input array, list the items associated with that value 
+// in config, if the item doesn't show as a config value, still show the item
+
+var RecursionExample = (function () {
+    var publicAPI,
+        input = ["dist"],
+        config = {
+            "dist": ["build", "deploy"],
+            "build": ["js", "css", "version-rev"],
+            "js": ["lint", "uglify"],
+            "css": ["sass", "css-min"]
+        };
+    tasks = [],
+    tasksWithRecursion = [];
+
+    function getTasks(input) {
+        //egghead.io lesson by Shane Osbourne
+        //https://egghead.io/lessons/javascript-intro-to-recursion-the-problem
+        
+        /*Inputs:
+          var input = ["dist"],
+              config = {
+                  "dist": ["build", "deploy"],
+                  "build": ["js", "css", "version-rev"],
+                  "js": ["lint", "uglify"],
+                  "css": ["sass", "css-min"]
+              },
+              tasks = [];*/
+
+        input.forEach(function (task) {
+            if (config[task]) {
+                config[task].forEach(function (task) {
+                    if (config[task]) {
+                        config[task].forEach(function (task) {
+                            if (config[task]) {
+                                config[task].forEach(function (task) {
+                                    tasks.push(task);
+                                })
+                            } else {
+                                tasks.push(task);
+                            }
+                        })
+                    } else {
+                        tasks.push(task);
+                    }
+                })
+            } else {
+                tasks.push(task);
+            }
+        });
+
+        return tasks;
+    };
+
+    function getTasksWithRecursion(input) {
+        //egghead.io lesson by Shane Osbourne
+        //https://egghead.io/lessons/javascript-intro-to-recursion-the-solution
+
+        /*Inputs:
+          var input = ["dist"],
+              config = {
+                  "dist": ["build", "deploy"],
+                  "build": ["js", "css", "version-rev"],
+                  "js": ["lint", "uglify"],
+                  "css": ["sass", "css-min"]
+              },
+              tasksWithRecursion = [];*/
+
+        input.forEach(function (task) {
+            if (config[task]) {
+                getTasksWithRecursion(config[task]);
+            } else {
+                tasksWithRecursion.push(task);
+            }
+        });
+
+        return tasksWithRecursion;
+    };
+        
+    publicAPI = {
+        getTasks: getTasks,
+        getTasksWithRecursion: getTasksWithRecursion
+    }
+
+    return publicAPI;
+
+})();
